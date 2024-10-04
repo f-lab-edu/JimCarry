43b35ae8c3303601eb1b8fd04bcfe2a935d9648e
hong <hong@DESKTOP-FCCHMNE>
2024-10-05 00:04:00 +0900

    1.행위(method)는 URL에 포함하지 않는다. AS-IS  TO-BE /save  ""
    2.PUT방식은 전체의 리소스를 교체 할 때 사용하고, PATCH는 리소스의 일부분을 교체 할 때 사용.
    AS-IS  TO-BE
    POST   PUT

--- JimCarry/src/main/java/com/study/jimcarry/controller/ConfirmQuotationController.java
+++ JimCarry/src/main/java/com/study/jimcarry/controller/ConfirmQuotationController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -48,7 +49,7 @@ public class ConfirmQuotationController {
 	 * @throws Exception
 	 */
     @ResponseBody
-    @PostMapping(value = "/save")
+    @PostMapping(value = "") //행위(method)는 URL에 포함하지 않는다.
     @Tag(name="ConfirmQuotation")
     @Operation(summary = "Insert ConfirmQuotation", description="견적확정 정보 저장")
 	public ResponseEntity<ConfirmQuotaionResponse> saveConfirmQuotation(@RequestBody @Valid ConfirmQuotationRequest reqeust, ConfirmQuotaionResponse response) throws Exception {
@@ -65,11 +66,15 @@ public class ConfirmQuotationController {
      * @throws Exception
      */
     @ResponseBody
-    @PostMapping(value = "/modify")
+    @PutMapping(value = "/{quotationid}") //PUT방식은 전체의 리소스를 교체 할 때 사용하고, PATCH는 리소스의 일부분을 교체 할 때 사용.
     @Tag(name="ConfirmQuotation")
     @Operation(summary = "Modify ConfirmQuotation", description="견적확정 정보 수정")
-	public ResponseEntity<ConfirmQuotaionResponse> modifyConfirmQuotation(@RequestBody @Valid ConfirmQuotationRequest reqeust, ConfirmQuotaionResponse response) throws Exception {
+	public ResponseEntity<ConfirmQuotaionResponse> modifyConfirmQuotation(
+			@PathVariable("quotationid") String quotationId,
+			@RequestBody @Valid ConfirmQuotationRequest reqeust, 
+			ConfirmQuotaionResponse response) throws Exception {
     	ConfirmQuotationEntity confirmQuotationEntity = modelMapper.map(reqeust, ConfirmQuotationEntity.class);
+    	confirmQuotationEntity.setReqQuotationId(quotationId);
     	confirmQuotationService.modifyConfrimQuotation(confirmQuotationEntity);
 		return new ResponseEntity<ConfirmQuotaionResponse>(response, HttpStatus.OK);
 	}
@@ -98,7 +103,7 @@ public class ConfirmQuotationController {
      * @throws Exception
      */
     @ResponseBody
-    @GetMapping(value = "/driver/{driverid}")
+    @GetMapping(value = "/drivers/{driverid}")
     @Tag(name="ConfirmQuotation")
     @Operation(summary = "get ConfirmQuotationList", description="기사님별 견적확정 정보 조회")
     public ResponseEntity<ConfirmQuotaionResponse> getConfirmQuotationList(@PathVariable("driverid") String driverId, ConfirmQuotaionResponse response) throws Exception {
@@ -115,7 +120,7 @@ public class ConfirmQuotationController {
      * @throws Exception
      */
     @ResponseBody
-    @GetMapping(value = "/customer/{customerid}")
+    @GetMapping(value = "/customers/{customerid}")
     @Tag(name="ConfirmQuotation")
     @Operation(summary = "get ConfirmQuotation", description="사용자별 견적 확정정보 조회")
     public ResponseEntity<ConfirmQuotaionResponse> getConfirmQuotation(@PathVariable("customerid") String customerId, ConfirmQuotaionResponse response) throws Exception {
--- JimCarry/src/main/java/com/study/jimcarry/controller/ReqQuotationController.java
+++ JimCarry/src/main/java/com/study/jimcarry/controller/ReqQuotationController.java
@@ -1,33 +1,30 @@
 package com.study.jimcarry.controller;
 
 import java.util.List;
-import java.util.Set;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.study.jimcarry.api.ReqQuotaionResponse;
 import com.study.jimcarry.api.ReqQuotationRequest;
 import com.study.jimcarry.domain.ReqQuotationEntity;
-import com.study.jimcarry.exception.CustomException;
-import com.study.jimcarry.exception.ErrorCode;
 import com.study.jimcarry.model.ReqQuotation;
 import com.study.jimcarry.service.ReqQuotationService;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.ConstraintViolation;
 import jakarta.validation.Valid;
 import jakarta.validation.Validation;
 import jakarta.validation.Validator;
@@ -35,7 +32,8 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Tag(name="ReqQuotation", description="ReqQuotation API")
-@Controller
+//@Controller
+@RestController
 @RequestMapping(value = "/api/req-quotation")
 public class ReqQuotationController {
 	
@@ -43,6 +41,8 @@ public class ReqQuotationController {
 	ReqQuotationService reqQuotationService;
 
 	Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
+	
+	//TODO Java @Bean으로 등록하기
 	private ModelMapper modelMapper = new ModelMapper();
 	
 	/**
@@ -53,10 +53,20 @@ public class ReqQuotationController {
 	 * @throws Exception
 	 */
     @ResponseBody
-    @PostMapping(value = "/save")
+    @PostMapping(value = "")//행위(method)는 URL에 포함하지 않는다.
     @Tag(name="ReqQuotaion")
     @Operation(summary = "Insert ReqQuotaion", description="견적요청서 저장")
 	public ResponseEntity<ReqQuotaionResponse> saveReqQuotation(@RequestBody @Valid ReqQuotationRequest reqeust, ReqQuotaionResponse response) throws Exception {
+    	
+    	//TODO 메소드 시그니처 측면에서 고민해볼 내용
+    	//TODO Response
+    	//response으로 인자를 받는 것이 어떤 차이가 있는지 알아볼것!
+    	//외부에서 인자가 어떻게 만들어지고 전달이 되는지 알아볼것!
+    	
+    	//TODO Exception
+    	//예외 처리 과정 공부하기
+    	
+    	//TODO 서블릿 컨테이너... 개념공부...
     	
     	//valid 체크 하고 싶은 필드가 있을 시 valids 배열에 기재
 //		String[] valids = {"ReqQuotationId"};
@@ -81,11 +91,14 @@ public class ReqQuotationController {
      * @throws Exception
      */
     @ResponseBody
-    @PostMapping(value = "/modify")
+    @PutMapping(value = "/{quotationid}") //PUT방식은 전체의 리소스를 교체 할 때 사용하고, PATCH는 리소스의 일부분을 교체 할 때 사용.
     @Tag(name="ReqQuotaion")
     @Operation(summary = "Modify ReqQuotaion", description="견적요청서 수정")
-	public ResponseEntity<ReqQuotaionResponse> modifyReqQuotation(@RequestBody @Valid ReqQuotationRequest reqeust, ReqQuotaionResponse response) throws Exception {
+	public ResponseEntity<ReqQuotaionResponse> modifyReqQuotation(@RequestBody @Valid ReqQuotationRequest reqeust, 
+			@PathVariable("quotationid") String quotationId,
+			ReqQuotaionResponse response) throws Exception {
 		ReqQuotationEntity reqQuotationEntity = modelMapper.map(reqeust, ReqQuotationEntity.class);
+		reqQuotationEntity.setReqQuotationId(quotationId);
 		reqQuotationService.modifyReqQuotation(reqQuotationEntity);
 		return new ResponseEntity<ReqQuotaionResponse>(response, HttpStatus.OK);
 	}
@@ -130,7 +143,7 @@ public class ReqQuotationController {
      * @throws Exception
      */
     @ResponseBody
-    @GetMapping(value = "/customer/{customerid}")
+    @GetMapping(value = "/customers/{customerid}")
     @Tag(name="ReqQuotaion")
     @Operation(summary = "get ReqQuotaion", description="사용자별 견적요청서 조회")
     public ResponseEntity<ReqQuotaionResponse> getReqQuotation(@PathVariable("customerid") String customerId, ReqQuotaionResponse response) throws Exception {
