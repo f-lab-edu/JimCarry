f6d3fcd51379a571d59f7589ab189006df3f45b5
hong <hong@DESKTOP-FCCHMNE>
2024-10-05 13:31:48 +0900

    브렌치 혼동으로 인한 재 Commit

--- JimCarry/src/test/java/com/study/jimcarry/mockup/ReqQuotationMockUpTest.java
+++ JimCarry/src/test/java/com/study/jimcarry/mockup/ReqQuotationMockUpTest.java
@@ -4,72 +4,70 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.study.jimcarry.JimCarryApplication;
 import com.study.jimcarry.api.ReqQuotationRequest;
-import com.study.jimcarry.controller.ReqQuotationController;
-import com.study.jimcarry.service.ReqQuotationService;
 
 import lombok.extern.slf4j.Slf4j;
 
-@WebMvcTest(ReqQuotationController.class) 
+@AutoConfigureMockMvc
+@SpringBootTest(classes= {JimCarryApplication.class})
+@Transactional
+@Rollback(false)
 @Slf4j
 public class ReqQuotationMockUpTest {
 
-    @Autowired
+	@Autowired
     private MockMvc mockMvc;
+	
+	@Test
+	public void saveReqQuotationTest() throws Exception {
+		ObjectMapper objectMapper = new ObjectMapper();
 
-    @MockBean // ReqQuotationService를 Mock으로 설정
-    private ReqQuotationService reqQuotationService;
-    
-    @MockBean
-    private ModelMapper modelMapper; // ModelMapper를 Mock으로 설정
-    
-    @Test
-    public void saveReqQuotationTest() throws Exception {
-        ObjectMapper objectMapper = new ObjectMapper();
+		ReqQuotationRequest request = new ReqQuotationRequest();
+	
+		String jsonString = objectMapper.writeValueAsString(request);
 
-        ReqQuotationRequest request = new ReqQuotationRequest();
-        // 필요한 request 데이터 설정
-        // request.setField(value); // 요청에 필요한 필드 값 설정
-
-        String jsonString = objectMapper.writeValueAsString(request);
-
-        MockHttpServletRequestBuilder builder = MockMvcRequestBuilders.post("/api/req-quotation")
-                .contentType("application/json;charset=UTF-8")
-                .content(jsonString);
-        
-        MvcResult mvcResult = mockMvc.perform(builder).andExpect(MockMvcResultMatchers.status().isOk())
-                .andDo(print())
-                .andReturn();
-        
-        Assertions.assertEquals(HttpStatus.OK.value(), mvcResult.getResponse().getStatus());
-        
-        String body = mvcResult.getResponse().getContentAsString();
-        log.debug("response: {}", body);
-    }
-
-    @Test
-    public void getReqQuotationListTest() throws Exception {
-        MockHttpServletRequestBuilder builder = MockMvcRequestBuilders.get("/api/req-quotation/list");
-        
-        MvcResult mvcResult = mockMvc.perform(builder).andExpect(MockMvcResultMatchers.status().isOk())
-                .andDo(print())
-                .andReturn();
-        
-        Assertions.assertEquals(HttpStatus.OK.value(), mvcResult.getResponse().getStatus());
-        
-        String body = mvcResult.getResponse().getContentAsString();
-        log.debug("response: {}", body);
-    }
+		MockHttpServletRequestBuilder builder = MockMvcRequestBuilders.post("/api/req-quotation/save")
+				.contentType("application/json;charset=UTF-8")
+				.content(jsonString);
+		
+		MvcResult mvcResult = mockMvc.perform(builder).andExpect(MockMvcResultMatchers.status().isOk())
+				.andDo(print())
+				.andReturn();
+		
+		Assertions.assertEquals(HttpStatus.OK.value(), mvcResult.getResponse().getStatus());
+		
+		String body = mvcResult.getResponse().getContentAsString();
+		log.debug("response: {}", body);
+	
+	}
+	
+	@Test
+	public void getReqQuotationListTest() throws Exception {
+		
+		MockHttpServletRequestBuilder builder = MockMvcRequestBuilders.get("/api/req-quotation/list");
+		
+		MvcResult mvcResult = mockMvc.perform(builder).andExpect(MockMvcResultMatchers.status().isOk())
+				.andDo(print())
+				.andReturn();
+		
+		Assertions.assertEquals(HttpStatus.OK.value(), mvcResult.getResponse().getStatus());
+		
+		String body = mvcResult.getResponse().getContentAsString();
+		log.debug("response: {}", body);
+		
+	}
 }
