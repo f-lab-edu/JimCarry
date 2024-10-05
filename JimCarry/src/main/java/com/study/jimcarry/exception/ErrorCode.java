310df4a4e77fe20e1afd13222dbf86452a20a856
hong <hong@DESKTOP-FCCHMNE>
2024-10-05 11:06:28 +0900

    [21번 이슈]

--- JimCarry/src/main/java/com/study/jimcarry/exception/ErrorCode.java
+++ JimCarry/src/main/java/com/study/jimcarry/exception/ErrorCode.java
@@ -1,26 +1,28 @@
 package com.study.jimcarry.exception;
 
+import org.springframework.http.HttpStatus;
+
 public enum ErrorCode {
-    NOT_FOUND(404, "리소스를 찾을 수 없음"),
-    UNAUTHORIZED(401, "권한이 없음"),
-    FORBIDDEN(403, "접근이 금지됨"),
-    INTERNAL_SERVER_ERROR(500, "내부 서버 오류"),
-    BAD_REQUEST(400, "잘못된 요청");
+	
+    NOT_FOUND(HttpStatus.NOT_FOUND, "리소스를 찾을 수 없음"),
+    UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "권한이 없음"),
+    FORBIDDEN(HttpStatus.FORBIDDEN, "접근이 금지됨"),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "내부 서버 오류"),
+    BAD_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청");
 
-	private int code;
+    private final HttpStatus status;
+    private final String message;
 
-	private String message;
+    private ErrorCode(HttpStatus status, String message) {
+        this.status = status;
+        this.message = message;
+    }
 
-	private ErrorCode(int code, String message) {
-		this.code = code;
-		this.message = message;
-	}
+    public int getCode() {
+        return status.value(); 
+    }
 
-	public int getCode() {
-		return code;
-	}
-
-	public String getMessage() {
-		return message;
-	}
+    public String getMessage() {
+        return message;
+    }
 }
